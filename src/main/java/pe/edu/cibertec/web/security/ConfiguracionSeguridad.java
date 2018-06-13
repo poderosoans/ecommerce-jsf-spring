e5a96eb2116bec619dda9bdccadf191ee82a9489
Roger Pomacarhua <poderosoans@hotmail.com>
2018-06-13 18:44:05 -0500

    Spring Security conectado a base de datos.

--- src/main/java/pe/edu/cibertec/web/security/ConfiguracionSeguridad.java
+++ src/main/java/pe/edu/cibertec/web/security/ConfiguracionSeguridad.java
@@ -1,27 +1,44 @@
 
 package pe.edu.cibertec.web.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter{
+    @Autowired
+    @Qualifier("userDetailsService")
+    UserDetailsService userDetailsService;
+    
+    @Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
                 .antMatchers("*/js/**").permitAll()
                 .antMatchers("*/css/**").permitAll()
                 .antMatchers("*/admin/**").hasAnyRole("ADMIN","CONTENT_MANAGER")
                 .antMatchers("/login.jsp").permitAll()
                 .antMatchers("/login").permitAll()
+                .antMatchers("/productos.xhtml").hasAnyRole("ROLE_ADMIN")
+                .antMatchers("/pasarela.xhtml").permitAll()
+                .antMatchers("addCarrito").permitAll()
                 .anyRequest().authenticated()
             .and()
                 .formLogin()
@@ -38,6 +55,13 @@ public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter{
     }
     
     @Bean
+	public PasswordEncoder passwordEncoder(){
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
+    
+        /*
+    @Bean
     @SuppressWarnings("deprecation")
     public UserDetailsService userDetailsService() {
         
@@ -51,4 +75,5 @@ public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter{
                 users.username("roger").password("roger123").roles("ADMIN").build());
         return manager;
     }
+*/
 }
---
+++ src/main/java/pe/edu/cibertec/web/security/MyUserDetailsService.java
@@ -0,0 +1,66 @@
+package pe.edu.cibertec.web.security;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.dominio.Usuario;
+import pe.edu.cibertec.service.UsuarioService;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.dominio.UserRole;
+
+
+@Service("userDetailsService")
+public class MyUserDetailsService implements UserDetailsService{
+    //get user from the database, via Hibernate
+	@Autowired
+    private UsuarioService usuarioService;
+        
+        
+    @Transactional(readOnly = true)
+    @Override
+    public UserDetails loadUserByUsername(final String username) 
+	throws UsernameNotFoundException {
+	
+	Usuario user = usuarioService.findByUserName(username);
+	List<GrantedAuthority> authorities = 
+                buildUserAuthority(user.getUserRole());
+
+	return buildUserForAuthentication(user, authorities);
+		
+    }
+    
+    // Converts com.mkyong.users.model.User user to
+	// org.springframework.security.core.userdetails.User
+	private User buildUserForAuthentication(Usuario user, 
+		List<GrantedAuthority> authorities) {
+		return new User(user.getUsername(), user.getPassword(), 
+			user.isEnabled(), true, true, true, authorities);
+	}
+
+	private List<GrantedAuthority> buildUserAuthority(Set<UserRole> userRoles) {
+
+		Set<GrantedAuthority> setAuths = new HashSet<GrantedAuthority>();
+
+		// Build user's authorities
+		for (UserRole userRole : userRoles) {
+			setAuths.add(new SimpleGrantedAuthority(userRole.getRole()));
+		}
+
+		List<GrantedAuthority> Result = new ArrayList<GrantedAuthority>(setAuths);
+
+		return Result;
+	}
+}
